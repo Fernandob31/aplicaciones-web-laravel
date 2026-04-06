21c3117623ecf515a2e67edfcc7acae4be0b4939
fernandob31 <benitezf.3105@gmail.com>
2026-04-06 18:04:26 -0300

    Fix: Pruebas de deploy

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
-require __DIR__ . '/../public/index.php';
+// Ver en qué carpeta estamos parados
+echo "Directorio actual: " . __DIR__ . "<br>";
+
+// Listar carpetas un nivel arriba
+echo "Contenido del nivel superior:<br>";
+$files = scandir(__DIR__ . '/..');
+foreach($files as $file) {
+    echo "- $file<br>";
+}
