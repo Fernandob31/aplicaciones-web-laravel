09e4909d4cc6f4369a8326fb2733c0ac8186310e
fernandob31 <benitezf.3105@gmail.com>
2026-04-06 18:07:47 -0300

    Fix: Pruebas de deploy

--- api/index.php
+++ api/index.php
@@ -1,10 +1,3 @@
 <?php
-// Ver en qué carpeta estamos parados
-echo "Directorio actual: " . __DIR__ . "<br>";
-
-// Listar carpetas un nivel arriba
-echo "Contenido del nivel superior:<br>";
-$files = scandir(__DIR__ . '/..');
-foreach($files as $file) {
-    echo "- $file<br>";
-}
+// Cargar el index real de Laravel desde la carpeta public
+require __DIR__ . '/../public/index.php';
