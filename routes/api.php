1e14dac971f25f2d94f8bdbd7bbf1d769dc8012a
Gaston Llampa <gastonsitto.ll@gmail.com>
2026-06-16 17:01:18 -0300

    Modificacion rutas API

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductoApiController;
 use App\Http\Controllers\Api\CategoriaApiController;
 
-//Route::get('/productos', [ProductoApiController::class, 'index']);
-//Route::get('/productos/{id}', [ProductoApiController::class, 'show']);
-Route::prefix('v1')->group(function () {
-    Route::get('/productos', [ProductoApiController::class, 'index']);
-    Route::get('/productos/{id}', [ProductoApiController::class, 'show']);
-    Route::get('/categorias', [CategoriaApiController::class, 'index']);
-    Route::get('/marcas', [ProductoApiController::class, 'marcas']);
-    Route::post('/pedidos', [PedidoApiController::class, 'store']);
-    Route::post('/pedidos/webhook', [PedidoApiController::class, 'webhook']);
-});
+Route::get('/productos', [ProductoApiController::class, 'index']);
+Route::get('/productos/{id}', [ProductoApiController::class, 'show']);
+Route::get('/categorias', [CategoriaApiController::class, 'index']);
+Route::get('/marcas', [ProductoApiController::class, 'marcas']);
+Route::post('/pedidos', [PedidoApiController::class, 'store']);
+Route::post('/pedidos/webhook', [PedidoApiController::class, 'webhook']);
